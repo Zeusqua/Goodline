d9f36da2c15266528f0bfcc5aab9fbea4726b4f0
Zeusqua <68560016+Zeusqua@users.noreply.github.com>
2021-02-28 22:57:57 +0700

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+	int a, b;
+	int otvet = 1;
+	cout << "enter number: "; cin >> a;
+	cout << "enter stepen: "; cin >> b;
+	for (int i = 0; i < b; i++)
+	{otvet = pow(a,b);}
+	cout << otvet;
+
+	return 0;
+}
